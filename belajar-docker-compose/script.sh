ac942853b0857c604fd9cb178c81c86d9738465b
Alvin Rig <alvinrigz@gmail.com>
2026-01-20 14:02:24 +0700

    Belajar docker compose

---
+++ belajar-docker-compose/script.sh
@@ -0,0 +1,10 @@
+# Menjalankan & memberhentikan docker compose
+docker compose create   # Membuat
+docker compose start    # Menjalankan
+docker compose stop     # Memberhentikan
+
+# Melihat Container 
+docker compose ps 
+
+# Menghapus container 
+docker compose down 
