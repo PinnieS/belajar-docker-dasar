cb3039e67eb5c8b5ccf4193e4ea7547f57679d67
Alvin Rig <alvinrigz@gmail.com>
2026-01-26 10:09:02 +0700

    update script

--- belajar-docker-compose/script.sh
+++ belajar-docker-compose/script.sh
@@ -9,11 +9,14 @@ docker compose ps
 # Menghapus container // volume dan image tidak ikut terhapus  
 docker compose down 
 
-# Kode project name
+docker compose down --rmi all # Menghapus container sekaligus semua imagenya
+
+# Kode project name (Melihat daftar project name yang sedang berjalan di Docker)
 docker compose ls
 
 # Melihat kejadian apa saja yang terjadi di dalem Docker secara realtime
 docker events --filter 'container=(nama container)'
+docker logs -f ( nama container) # Melihat isi catatan sistem/log aplikasi untuk mencari error
 
 # Konfigurasi Multi-Lingkungan (Environment Overrides) // Kode extend service //Multi-environments
 docker compose -f dev.yaml 
@@ -21,4 +24,8 @@ docker compose -f docker-compose.yaml -f dev.yaml create
 docker compose -f docker-compose.yaml -f dev.yaml start
 
 
-
+# Perintah Build & Eksekusi 
+docker compose build # Membangun (memasak) Image dari Dockerfile
+docker compose up # # Membuat + Menjalankan
+docker compose up -d # Menjalankan di Background / Detached mode
+docker compose up --build # Paksa build ulang image sebelum menjalankan
